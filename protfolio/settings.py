38a09c547f73d7ee42dd76fa92a00e85cf0639f9
B-nod <binodtamang245@gmail.com>
2024-02-29 14:26:00 +0545

    dj

--- protfolio/settings.py
+++ protfolio/settings.py
@@ -104,19 +104,28 @@ WSGI_APPLICATION = 'protfolio.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
-DATABASES = {
-    'default':{
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'Name':'ddpb2bndvgsbcl',
-        'USER': 'qdgwhoufkopfrf',
-        'PASSWORD': 'f6c84dac247fe124311bcdace34134eec54c1eb940ff8f599018e8d4627a16f3',
-        'HOST':'ec2-35-169-9-79.compute-1.amazonaws.com',
-        'PORT': '5432'
+if IS_HEROKU_APP:
+    DATABASES = {
+        'default':{
+            'ENGINE': 'django.db.backends.postgresql_psycopg2',
+            'Name':'ddpb2bndvgsbcl',
+            'USER': 'qdgwhoufkopfrf',
+            'PASSWORD': 'f6c84dac247fe124311bcdace34134eec54c1eb940ff8f599018e8d4627a16f3',
+            'HOST':'ec2-35-169-9-79.compute-1.amazonaws.com',
+            'PORT': '5432'
+        }
+        
     }
-    
+else:
+     DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
 }
 
-DATABASES['default'] = dj_database_url.config(conn_max_age=600, ssl_require=True)
+db_from_env = dj_database_url.config(conn_max_age=600)
+DATABASES['default'].update(db_from_env)
 
 # if IS_HEROKU_APP:
 #     # In production on Heroku the database configuration is derived from the `DATABASE_URL`
